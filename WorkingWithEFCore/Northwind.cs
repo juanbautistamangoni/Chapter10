2ce428f44e0e178234fdd4569b34d85e97014c06
JuanBautistaMangoni <jmangoni@uade.edu.ar>
2024-04-25 00:35:03 -0300

    Add project files.

---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Packt.Shared;
+public class Northwind : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Nortwind.db");
+        string connection = $"Filename={path}";
+
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"Connection: {connection}");
+        ForegroundColor = previousColor;
+
+        optionsBuilder.UseSqlite(connection);
+
+    }
+}
+
---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,7 @@
+﻿using Packt.Shared;
+
+Northwind db = new();
+
+WriteLine($"Provider: {db.Database.ProviderName}");
+
+
